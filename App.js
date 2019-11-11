36b1682e39452712196d9db7e0e2f0bbe59bb2ba
AARON M <asmarcks@gmail.com>
2019-11-10 18:10:52 -0600

    it works yay

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+  }
+
 navigator.getUserMedia =
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -15,14 +23,26 @@ handTrack.startVideo(video)
         if (status) {
             navigator.getUserMedia({ video: {} }, stream => {
                 video.srcObject = stream;
+                setInterval(runDetection, 1000);
             },
                 err => console.log(err),
 
             );
         }
-    })
+    });
 
-handTrack.load()
+    function runDetection(){
+        model.detect(video)
+        .then(predictions => {
+            console.log(predictions);
+            model.renderPredictions(predictions, canvas, context, video);
+            if(predictions.length > 0){
+                audio.play();
+            }
+        });
+    }
+
+handTrack.load(modelParams)
     .then(lmodel => {
         model = lmodel;
     });
